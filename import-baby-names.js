41b5d78acc52ffd6a0fbe49481a26cdcafa0057a
Khadijah Okoh <keodijah@Khadijahs-MacBook-Pro.local>
2017-08-07 20:55:29 -0400

    final commit

--- import-baby-names.js
+++ import-baby-names.js
@@ -5,7 +5,7 @@ fs.readFile('baby-names.csv', function(err,data){
         throw err
     }
 
-    console.log(data.toString('utf-8'))
+   // console.log(data.toString('utf-8'))
     
     //to write to the file
     /*fs.writeFile('baby-name-copy.csv', 'hey world!', function(err){
@@ -13,4 +13,20 @@ fs.readFile('baby-names.csv', function(err,data){
             console.log(err)
         }
     })*/
+//console.log(parseData.split('\n')//splits data into array seats
+var parsedData = data.toString('utf-8')
+var result = parsedData.split('\n').slice(1).map(function(intel){
+    var pieceOfData = intel.split(',')
+    return{
+        birthYear: parseInt(pieceOfData[0]),
+        gender: pieceOfData[1],
+        ethnicity:  pieceOfData[2],
+        name: pieceOfData[3],
+        count: parseInt(pieceOfData[4]),
+        rank: parseInt(pieceOfData[5]),
+
+
+    }
+})
+console.log(result[0])
 })
